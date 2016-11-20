c780daee1cb61dcfd1562073888812c406e22f11
Toni <Tonizziva@hotmail.com>
2016-11-20 16:39:27 +0200

    jtyj
    rur

--- main.js
+++ main.js
@@ -33,3 +33,17 @@ function checkCookie() {
        }
     }
 }
+
+function() {
+  var cards = document.querySelectorAll(".flip-container.click");
+  for ( var i  = 0, len = cards.length; i < len; i++ ) {
+    var card = cards[i];
+    clickListener( card );
+  }
+
+  function clickListener(card) {
+    card.addEventListener( "click", function() {
+      var c = this.classList;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+    });
+  }
