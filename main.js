842c6f0f185e1e17f84356a1edcd5c6eaf0f5211
Tonizziva <Tonizziva@hotmail.com>
2016-11-25 11:58:02 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function setCookie(lk,lv,exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires=" + d.toGMTString();
-    document.cookie = lk + "="+ lv + expires + ";path=/";
+    document.cookie = lk + "="+ lv+ ";" + expires + ";path=/";
 }
 
 function getCookie(lname) {
@@ -19,21 +19,39 @@ function getCookie(lname) {
     }
     return "";
 }
-
-function checkCookie() {
-    var lk=getCookie("username");
+function load(b) {
+    var o = document.getElementsByClassName("flip-container");
+    var i;
+    for (i = 0; i < o.length; i++) {
+        if(b[i] == 1) {
+            o[i].className += " hover";
+        }
+    }
+}
+        
+var lk=getCookie("username");
+function checkCookie(l) {
+    
     if (lk != "") {
         var b = lk.split(',');
-        alert(b[20]);
+        load(b);
     } else {
-       lk = "0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0;";
+        lk = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0;";
        if (lk != "" && lk != null) {
            setCookie("username", lk, 30);
        }
     }
 }
+function cookie(l,a) {
+    var lista = lk.split(',');
+    lista[l] = a;
+    console.log(l,a);
+    jono = lista.join();
+    setCookie("username", jono, 30);
+    var b = lk.split(',');
+        load(b);
+}
 
-function avaus(l) {
-    b[0] = l;
-    alert(b[0]);
+function avaus(l,a) {
+    cookie(l,a);
 }
