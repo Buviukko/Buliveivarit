e4713d5f83058a53a6355d345211465f53d1c443
Tonizziva <Tonizziva@hotmail.com>
2016-12-05 19:26:40 +0200

    Final version
    Toimiva ilman sisältöö

--- main.js
+++ main.js
@@ -1,57 +1,29 @@
-function setCookie(lk,lv,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = lk + "="+ lv+ ";" + expires + ";path=/";
+var d = new Date();
+if(document.cookie == "") {
+    var luukut = ["0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"];
+    document.cookie = luukut;
+} else {
+    var c = document.cookie;
+    var lk = c.split(",");
+    load(lk);
 }
-
-function getCookie(lname) {
-    var name = lname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-function load(b) {
+function load(l) {
+    document.getElementById("l"+d.getDate()).style.boxShadow = "0px 0px 62px 15px rgba(255,0,0,1)";
     var o = document.getElementsByClassName("flip-container");
-    var i;
-    for (i = 0; i < o.length; i++) {
-        if(b[i] == 1) {
-            o[i].className += " hover";
+        for (var i=0; i < o.length; i++) {
+            if(l[i] == 1) {
+                o[i].className += " hover";
+            }
         }
+}
+function avaus(l,v) {
+    if((l+1) <= d.getDate()) {
+        v.classList.toggle('hover');
+        if(lk[l] == 0) {
+            lk[l] = 1;
+        } else {
+            lk[l] = 0;
+        }
+        document.cookie = lk;
     }
-}
-        
-var lk=getCookie("username");
-function checkCookie(l) {
-    
-    if (lk != "") {
-        var b = lk.split(',');
-        load(b);
-    } else {
-        lk = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0;";
-       if (lk != "" && lk != null) {
-           setCookie("username", lk, 30);
-       }
-    }
-}
-function cookie(l,a) {
-    var lista = lk.split(',');
-    lista[l] = a;
-    console.log(l,a);
-    jono = lista.join();
-    setCookie("username", jono, 30);
-    var b = lk.split(',');
-        load(b);
-}
-
-function avaus(l,a) {
-    cookie(l,a);
 }
